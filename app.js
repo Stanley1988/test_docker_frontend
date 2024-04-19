75ed261b04c2000e018419cff1596dbf889c863f
Stanley Mtonyole <stanleymtonyole@gmail.com>
2024-04-19 12:20:04 +0300

    New

---
+++ app.js
@@ -0,0 +1,50 @@
+// Import the Express module
+const { default: axios } = require('axios');
+const express = require('express');
+const baseUrl = process.env.BASE_URL || 'http://localhost';
+const b1 = process.env.B1PORT || 5000;
+const b2 = process.env.B2PORT  || 5001;
+
+
+// Create an Express application
+const app = express();
+
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Front end');
+});
+
+app.get('/compareNumber/:value', async (req, res) => {
+  let output = 'No data';
+  await axios.get(baseUrl + ':'+b2 +'/compare/' + req.params.value).then(function (response) {
+    // console.log('success itl',response);
+    // console.log('success itl',response.data);
+    output = response.data;
+  })
+    .catch(function (error) {
+      console.log('error',error);
+      //console.log('error itl',error);
+    });
+  res.send(output);
+});
+
+app.get('/incrementNumber/:value', async (req, res) => {
+  let output = 'No data';
+  await axios.get(baseUrl + ':'+b1 +'/increment/' + req.params.value).then(function (response) {
+    // console.log('success itl',response);
+    // console.log('success itl',response.data);
+    output = response.data;
+  })
+    .catch(function (error) {
+      //console.log('error',error);
+      //console.log('error itl',error);
+    });
+  res.send(output);
+});
+
+
+// Start the server
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
